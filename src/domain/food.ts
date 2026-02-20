cc3c7acbea5a9fa4a2ce245f2d749258e4ab7d1d
intellic <intellic@student.21-school.ru>
2026-02-20 22:07:31 +0500

    build: Food class

---
+++ src/domain/food.ts
@@ -0,0 +1,21 @@
+import Item from "./item.js";
+import Character from "./character.js";
+
+abstract class Food extends Item {
+    #healPercent: number;
+
+    constructor(name: string, healPercent: number) {
+        super(name);
+        this.#healPercent = healPercent;
+    }
+
+    get healPercent(): number {
+        return this.#healPercent;
+    }
+
+    eat(character: Character) {
+        character.heal(this.#healPercent);
+    }
+}
+
+export default Food;
