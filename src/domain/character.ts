e35495a27177595995731ca2ec719559775c6f00
intellic <intellic@student.21-school.ru>
2026-02-20 18:35:37 +0500

    build: Character class

---
+++ src/domain/character.ts
@@ -0,0 +1,56 @@
+class Character {
+    #currentHealth: number;
+    #maxHealth: number;
+    #strength: number;
+    #agility: number;
+    // #position: Position;
+    // #equippedWeapon: Weapon;
+    // #inventory: Inventory;
+
+    constructor() {
+        this.#maxHealth = 100;
+        this.#currentHealth = 100;
+        this.#strength = 10;
+        this.#agility = 10;
+    }
+
+    takeDamage(amount: number) {
+        this.#currentHealth -= amount;
+    }
+
+    heal(amount: number) {
+        if (this.#currentHealth + amount > this.#maxHealth) {
+            this.#currentHealth = this.#maxHealth;
+        } else {
+            this.#currentHealth += amount;
+        }
+    }
+
+    increaseMaxHealth(amount: number) {
+        this.#maxHealth += amount;
+        this.#currentHealth += amount;
+    }
+
+    decreaseMaxHealth(amount: number) {
+        this.#maxHealth = this.#maxHealth > amount ? this.#maxHealth - amount : 1;
+    }
+
+    // useScroll(scroll: Scroll) {
+    //     scroll.apply(this);
+    // }
+
+    // eatFood(food: Food) {
+    //     food.apply(this);
+    // }
+
+    // drinkPotion(potion: Potion) {
+    //     potion.apply(this);
+    // }
+
+    // useItem(item: Item) {
+
+    // }
+
+}
+
+export default Character;
