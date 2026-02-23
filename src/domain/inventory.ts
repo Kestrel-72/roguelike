94a5b7a2a75bb42832273246d7652b3c92ac8ffe
intellic <intellic@student.21-school.ru>
2026-02-23 18:51:01 +0500

    build: Inventory class

---
+++ src/domain/inventory.ts
@@ -0,0 +1,50 @@
+import Character from "./character.js";
+import Weapon from "./items/weapon.js";
+import Item from "./items/item.js";
+import Consumable from "./items/consumable.js";
+
+class Inventory {
+    #itemSlots: ItemSlot[] = [];
+    #numSlots: number;
+    #character: Character;
+
+    constructor(character: Character) {
+        this.#numSlots = 9;
+        this.#character = character;
+    }
+
+}
+
+class ItemSlot {
+    #item: Item | null;
+    #quantity: number;
+
+    constructor() {
+        this.#item = null;
+        this.#quantity = 0;
+    }
+
+    // add items into the slot. If final quantity exceeds stack_size, discard the rest
+    addItem(item: Item, addQuantity: number = 1): void {
+        if (this.#item === null) {
+            this.#item = item;
+            this.#quantity = addQuantity;
+        } else if (this.#item.id === item.id) {
+            const spaceLeft = this.#item.stack_size - this.#quantity;
+            this.#quantity += Math.min(addQuantity, spaceLeft);
+        }
+    }
+
+    useItem(character: Character): void {
+        if (this.#item instanceof Weapon) {
+            const tmp = character.equippedWeapon;
+            character.equippedWeapon = this.#item;
+            this.#item = tmp;
+        }
+        if (this.#item instanceof Consumable) {
+            this.#item.use(character);
+        }
+    }
+}
+
+export default Inventory;
