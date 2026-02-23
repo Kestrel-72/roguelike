2afe0fc407106f76c10d7a7037480a8018117fc5
intellic <intellic@student.21-school.ru>
2026-02-23 18:50:09 +0500

    unite Scroll into one class

--- src/domain/items/scroll.ts
+++ src/domain/items/scroll.ts
@@ -1,12 +1,45 @@
 import Character from "../character.js";
-import Item from "./item.js";
+import Consumable from "./consumable.js";
 
-abstract class Scroll extends Item {
-    constructor(id: number, name: string) {
-        super(id, name, 9);
+class Scroll extends Consumable {
+    #template: typeof ScrollType[keyof typeof ScrollType];
+
+    constructor(type: keyof typeof ScrollType) {
+        const template = ScrollType[type];
+        super(template.id, template.name);
+        this.#template = template;
     }
 
-    abstract use(character: Character): void;
+    use(character: Character) {
+        this.#template.use(character);
+    }
+}
+
+const ScrollType = {
+    AGILITY: {
+        id: 11,
+        name: "Scroll of Agility",
+        boostAmount: 1,
+        use(character: Character) {
+            character.increaseAgility(this.boostAmount);
+        }
+    },
+    STRENGTH: {
+        id: 12,
+        name: "Scroll of Strength",
+        boostAmount: 1,
+        use(character: Character) {
+            character.increaseStrength(this.boostAmount);
+        }
+    },
+    HEALTH: {
+        id: 13,
+        name: "Scroll of Health",
+        boostAmount: 10,
+        use(character: Character) {
+            character.increaseMaxHealth(this.boostAmount);
+        }
+    }
 }
 
 export default Scroll;
